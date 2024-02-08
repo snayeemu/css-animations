fed286f1e14986d158ba5e458fd97f7e13f7befd
snayeemu <snayeemu2@gmail.com>
2024-02-08 20:42:39 +0600

    rotate added

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="main">
       <div className="box-1"></div>
-      <h1>Something Cool</h1>
+      <div className="box-2-container">
+        <div className="box-2"></div>
+      </div>
     </div>
   );
 }
