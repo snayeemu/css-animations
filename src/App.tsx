13ce3b4db08aba788f14c7c5bd7f7b4ca856ddd4
snayeemu <snayeemu2@gmail.com>
2024-02-08 21:27:49 +0600

    moving animation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="main">
-      <button className="btn">Hover me</button>
+      <div className="box"></div>
     </div>
   );
 }
