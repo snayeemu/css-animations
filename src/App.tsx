7162804c33bab644e316cd6a9899077d5dfaf769
snayeemu <snayeemu2@gmail.com>
2024-02-09 20:46:54 +0600

    animation added using clip path

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="main">
-      <div className="loading">
+      {/* <div className="loading">
         <div className="glass"></div>
         <div className="box"></div>
+      </div> */}
+      <div className="card">
+        <h1>Heading</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nam
+          culpa a adipisci, deleniti temporibus vel aperiam itaque dicta
+          blanditiis minus repudiandae voluptas reprehenderit, totam aspernatur
+          magni dignissimos veritatis. Est.
+        </p>
       </div>
     </div>
   );
